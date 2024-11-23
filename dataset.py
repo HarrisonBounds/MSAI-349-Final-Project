c1eaa977e6e9d6b06362154a37fa42ffe9a224ab
Harrison Bounds <harrison.bounds777@gmail.com>
2024-11-23 10:54:26 -0600

    Custom Dataset class added

---
+++ dataset.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import torch
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None):
+        self.annotations = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.annotations.iloc[idx, 0])
+        image = read_image(img_path)
+        label = torch.tensor(int(self.img_labels.iloc[idx, 1]))
+        if self.transform:
+            image = self.transform(image)
+        return image, label
