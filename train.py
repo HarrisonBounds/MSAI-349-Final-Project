06588a376a8fb3d17e8815b99bb4df356e61b477
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2024-11-25 18:12:52 -0600

    Added model testing and saving loop.

--- train.py
+++ train.py
@@ -4,42 +4,45 @@ from dataset import CustomImageDataset
 from torch.utils.data import DataLoader
 from cnn import CNN
 import torch.nn as nn
+from PIL import Image
+# from sketch_interface import Interface
 
+
+# Interface = Interface()
+# Interface.draw()
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {DEVICE} device")
 
-#Loss lists for plotting
+# Loss lists for plotting
 train_losses = []
 valid_losses = []
 test_losses = []
 
-#Hyperparameters
+# Hyperparameters
 batch_size = 32
 image_width = 300
 image_height = 300
 lr = 0.001
 epochs = 20
 
+# Loading and pre-processing data
 transform = transforms.Compose([
     transforms.Resize((image_width, image_height)),
 ])
 
 dataset = CustomImageDataset("sketches.csv", "data", transform)
-
-train_set, valid_set, test_set = torch.utils.data.random_split(dataset, [0.7, 0.15, 0.15])
+train_set, valid_set = torch.utils.data.random_split(dataset, [0.75, 0.25])
+test_image = "user_sketch.png" 
 
 train_loader = DataLoader(train_set, batch_size, shuffle=True)
 valid_loader = DataLoader(valid_set, batch_size, shuffle=True)
-test_loader = DataLoader(test_set, batch_size, shuffle=True)
 
 model = CNN(image_width, image_height).to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr) #Tune this for potential better results
 loss_func = nn.CrossEntropyLoss()
 
 #Training Loop
-
-
 for i in range(epochs):
     print(f"Epoch {i}\n=========================")
     model.train()
@@ -55,12 +58,49 @@ for i in range(epochs):
         batch_loss.backward()
         optimizer.step()
         
-        num_samples_processed += y_true.shape[0]
-        
-        print(f"Batch Loss: {batch_loss.item()} [{num_samples_processed}/{len(train_loader.dataset)}]")
-        
+        num_samples_processed += y_true.shape[0]        
+        # print(f"Batch Loss: {batch_loss.item()} [{num_samples_processed}/{len(train_loader.dataset)}]")        
         epoch_loss = (epoch_loss * b + batch_loss.item()) / (b + 1)
-        
+        torch.save(model.state_dict(), f"model_epoch_{i}.pth")
+
+# Save the final model.
+torch.save(model.state_dict(), "final_model.pth")
+print("Final model saved to 'final_model.pth'")
+
+# Validation loop
+# Visualisation section
+
+# Testing loop
+
+test_transform = transforms.Compose([
+    transforms.Resize((image_width, image_height)),  # Resize to model's input size
+    transforms.ToTensor(),  # Convert PIL to tensor
+])
+
+# Load the trained model
+model.load_state_dict(torch.load("final_model.pth"))  
+model.eval()  # Set the model to evaluation mode
+print("testing loop...")
+# Process the image and make a prediction
+with torch.no_grad():  # Disable gradient computation for inference
+    # Load and preprocess the image
+    image = Image.open(test_image).convert("L") 
+    image = test_transform(image)
+    image = image.unsqueeze(0).to(DEVICE)  # Add one axis to the batch dimension
+
+    # Get the model's prediction
+    output = model(image) 
+    # Get the class index with the highest score
+    predicted_label = torch.argmax(output, dim=1).item()  
+    print(f"Predicted Label for '{test_image}': {predicted_label} : ")
+
+    for key, value in dataset.label_map.items():
+        if value == predicted_label:
+            key_for_value = key
+            print(f"the image is of : {key}")
+            break
+
+   
         
         
         
