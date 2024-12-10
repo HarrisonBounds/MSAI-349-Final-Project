eb669d34e76532df0fe05e3be99787d3adb511f8
Harrison Bounds <harrison.bounds777@gmail.com>
2024-12-10 11:58:44 -0600

    Update hyperparameters

--- cnn.py
+++ cnn.py
@@ -8,25 +8,31 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=(5, 5), stride=(2,2))
         self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(1,1))
         
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
         #Calculate image size after the convolutional layers
         #output size: (input_size + 2*padding_size - kernel_size) / stride_size + 1
         input_size = (width, height)
         size_1 = (((input_size[0] - 5) // 2) + 1), (((input_size[1] - 5) // 2) + 1)
-        size_2 = (((size_1[0] - 3) // 1) + 1), (((size_1[1] - 3) // 1) + 1)
+        size_1_pool = (size_1[0] // 2, size_1[0] // 2)
+        size_2 = (((size_1_pool[0] - 3) // 1) + 1), (((size_1_pool[1] - 3) // 1) + 1)
+        size_2_pool = (size_2[0] // 2, size_2[0] // 2)
         
-        fc_input = size_2[0] * size_2[1] * 64
+        fc_input = size_2_pool[0] * size_2_pool[1] * 64
         
-        self.fc1 = nn.Linear(fc_input, 512)
-        self.fc2 = nn.Linear(512, 360)
-        self.fc3 = nn.Linear(360, 250)
+        self.fc1 = nn.Linear(fc_input, 960)
+        self.fc2 = nn.Linear(960, 512)
+        self.fc3 = nn.Linear(512, 250)
         
-        self.dropout = nn.Dropout(0.2)
-        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.3)
+        self.relu = nn.LeakyReLU()
         self.flatten = nn.Flatten()
         
     def forward(self, x):
        x = self.relu(self.conv1(x))
+       x = self.pool(x)
        x = self.relu(self.conv2(x))  
+       x = self.pool(x)
     
        x = self.flatten(x)
        x = self.relu(self.fc1(x))
