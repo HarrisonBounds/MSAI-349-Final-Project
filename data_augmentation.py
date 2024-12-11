523113c236d5c7069263490523d926ff5fbf3f68
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2024-12-10 22:38:34 -0600

    Best model

--- data_augmentation.py
+++ data_augmentation.py
@@ -2,14 +2,14 @@ from PIL import Image
 import os
 
 # Take data file as input
-input_folder_name = "mnist_data/trainingSet/trainingSet"  #for mnist data
+input_folder_name = "data"  # for mnist data
 # input_folder_name = "data"  #for data folder
 
 # Create the modified folder.
 modified_folder_name = f"modified_{input_folder_name}"
 try:
-    os.makedirs(modified_folder_name, exist_ok=True)  
-    print(f"Folder created: {modified_folder_name}")
+    os.makedirs(modified_folder_name, exist_ok=True)
+    # print(f"Folder created: {modified_folder_name}")
 except OSError as e:
     print(f"Error creating folder: {e}")
 
@@ -17,18 +17,18 @@ except OSError as e:
 # Loop through all subdirectories and files
 for root, dirs, files in os.walk(input_folder_name):
     for file in files:
-        
 
-        if file.lower().endswith(('.png','.jpg')):  # Check for image files
+        if file.lower().endswith(('.png', '.jpg')):  # Check for image files
             input_file_name = file
-            input_sub_fold = root.replace(f"{input_folder_name}/","")
+            input_sub_fold = root.replace(f"{input_folder_name}/", "")
             input_file_path = os.path.join(root, file)
 
             # create sub-folder if not already present.
             try:
-                sub_fold_path = os.path.join(modified_folder_name,input_sub_fold)
-                os.makedirs(sub_fold_path, exist_ok=True)  
-                print(f"Folder created: {sub_fold_path}")
+                sub_fold_path = os.path.join(
+                    modified_folder_name, input_sub_fold)
+                os.makedirs(sub_fold_path, exist_ok=True)
+                # print(f"Folder created: {sub_fold_path}")
             except OSError as e:
                 print(f"Error creating folder: {e}")
 
@@ -40,11 +40,11 @@ for root, dirs, files in os.walk(input_folder_name):
             # Save the rotated images
             for angle in angles:
                 rotated_image = original_image.rotate(angle, expand=True)
-                output_file_path_root = os.path.join(modified_folder_name, input_sub_fold)
-                print(f"root: {output_file_path_root}")
-                output_file_name = f"modified_{input_file_name}_{angle}.png" 
-                output_file_path = os.path.join(output_file_path_root , output_file_name)
-                print(f"Saved rotated image: {output_file_path}")
+                output_file_path_root = os.path.join(
+                    modified_folder_name, input_sub_fold)
+                # print(f"root: {output_file_path_root}")
+                output_file_name = f"modified_{input_file_name}_{angle}.png"
+                output_file_path = os.path.join(
+                    output_file_path_root, output_file_name)
+                # print(f"Saved rotated image: {output_file_path}")
                 rotated_image.save(output_file_path)
-
-
